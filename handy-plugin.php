455cc1a7588a48babbd85b686baeb438d0e95b80
J.J. Oli <olijj@go.byuh.edu>
2018-10-09 02:06:34 -0600

    start

---
+++ handy-plugin.php
@@ -0,0 +1,14 @@
+<?php
+/**
+* @package HandyPlugin
+*/
+/*
+Plugin Name: Handy Plugin
+Plugin URI: https://github.com/Olimejj/plugin1.git
+Description: This is a very handy plugin.
+Version: 1.0.0
+Author: J.J. Oliphant
+Author: URI: olimejj.com
+License: GPLv2 or later
+Text Domain: handy-plugin
+*/
